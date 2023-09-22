43d22bf55a8464bf11d2fcf00a73e325ffde2250
Korobenko2023 <vivika3110@gmail.com>
2023-09-22 11:54:49 +0300

    fix task

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -13,14 +13,33 @@ export function fetchBreeds() {
 }
 
 export function fetchCatByBreed(breedId) {
-    const BASE_URL = 'https://api.thecatapi.com/v1/images/search?breed_ids={breed.id}';
-    return axios.get(BASE_URL)
-.then(response => { 
-    return response.data;
-  })
-.catch(error => {
-    throw error;
-  });
+    const API_KEY = "live_AjlxCbLIu6QxMy1La6yi4tYaWd7WEJMxrplNzXLhmLw0KTUQp9bBtpZcf4znrVR6";
+    const BASE_URL = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
+    return axios.get(BASE_URL, {
+        headers: {
+            'x-api-key': API_KEY
+        }
+    })
+    .then(response => { 
+        console.log(response);
+        return response.data;
+    })
+    .catch(error => {
+        throw error;
+    });
 }
+
+
+// export function fetchCatByBreed(breedId) {
+//     const BASE_URL = 'https://api.thecatapi.com/v1/images/search?breed_ids={breed.id}';
+//     return axios.get(BASE_URL)
+// .then(response => { 
+//     console.log(response);
+//     return response.data;
+//   })
+// .catch(error => {
+//     throw error;
+//   });
+// }
 
 
--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,6 @@ import { fetchBreeds, fetchCatByBreed } from './cat-api';
 import Notiflix from 'notiflix';
 
 const breedSelect = document.getElementById('breed-select');
-const searchForm = document.getElementById('search-form');
 const catInfo = document.querySelector('.cat-info');
 
 fetchBreeds()
@@ -18,18 +17,25 @@ fetchBreeds()
     Notiflix.Notify.failure('Error fetching breeds:', error);
 });
 
-searchForm.addEventListener('submit', event => {
+breedSelect.addEventListener('change', event => {
     event.preventDefault();
     const selectedBreedId = breedSelect.value; 
      
 fetchCatByBreed(selectedBreedId)
- .then((catData) => {   
+ .then((catData) => { 
+    const breedInfo = catData[0].breeds[0];  
     catInfo.innerHTML = `
-                <p>Breed: ${catData[0].breeds[0].name}</p>
-                <p>Description: ${catData[0].breeds[0].description}</p>
-                <p>Temperament: ${catData[0].breeds[0].temperament}</p>
-                <img src="${catData[0].url}" alt="${catData[0].breeds[0].name}">
+                <p>Breed: ${breedInfo.name}</p>
+                <p>Description: ${breedInfo.description}</p>
+                <p>Temperament: ${breedInfo.temperament}</p>
+                <img src="${catData[0].url}" alt="${breedInfo.name}">
             `;
+    // catInfo.innerHTML = `
+    //             <p>Breed: ${catData[0].breeds[0].name}</p>
+    //             <p>Description: ${catData[0].breeds[0].description}</p>
+    //             <p>Temperament: ${catData[0].breeds[0].temperament}</p>
+    //             <img src="${catData[0].url}" alt="${catData[0].breeds[0].name}">
+    //         `;
 })
  .catch(error => {
   Notiflix.Notify.failure('Error fetching cat info:', error);
