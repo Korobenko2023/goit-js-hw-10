e96ecc7af9b21aa6effbf5d36eed016e86d65398
Korobenko2023 <vivika3110@gmail.com>
2023-09-24 22:54:00 +0300

    fix task

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,12 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 import Notiflix from 'notiflix';
 
-const breedSelect = document.getElementById('breed-select');
-const catInfo = document.querySelector('.cat-info');
-const loader = document.querySelector('.loader');
-const error = document.querySelector('.error');
+const refs = {
+    breedSelect: document.querySelector('.breed-select'),
+    loader: document.querySelector('.loader'),
+    error: document.querySelector('.error'),
+    catInfo: document.querySelector('.cat-info'),
+};
   
 fetchBreeds()
 .then((breeds) => {       
@@ -12,30 +14,28 @@ fetchBreeds()
         return `<option value="${id}">${name}</option>`
     }).join("");
    
-    breedSelect.insertAdjacentHTML('afterbegin', markup);
+    refs.breedSelect.insertAdjacentHTML('afterbegin', markup);
 
-    breedSelect.classList.remove('is-hidden'); 
-
+    refs.breedSelect.classList.remove('is-hidden'); 
 })
 .catch(error => {
     Notiflix.Notify.failure('Error fetching cat info: ', error);         
 })
 .finally(() => {        
-    loader.style.display = 'none';  
+    refs.loader.style.display = 'none';  
 });
 
-breedSelect.addEventListener('change', event => {
+ refs.breedSelect.addEventListener('change', event => {
     event.preventDefault();
-    const selectedBreedId = breedSelect.value;  
-    loader.classList.remove('is-hidden');  
-    loader.style.display = 'block';  
-    catInfo.innerHTML = '';
+    const selectedBreedId = refs.breedSelect.value;  
+    refs.loader.style.display = 'block';  
+    refs.catInfo.innerHTML = '';
           
 fetchCatByBreed(selectedBreedId)
  .then((catData) => { 
     if (catData.length > 0) {   
     const breedInfo = catData[0].breeds[0];  
-    catInfo.innerHTML = `
+    refs.catInfo.innerHTML = `
         <img class= "cat-info-img" src="${catData[0].url}" width="300" alt="${breedInfo.name}">
         <div class= "cat">
         <h2>${breedInfo.name}</h2>
@@ -43,18 +43,77 @@ fetchCatByBreed(selectedBreedId)
         <p class= "cat-info-text-bold">Temperament: <span class= "cat-info-text-temp">${breedInfo.temperament}</span></p>
         </div>                
     `;
-    catInfo.classList.remove('is-hidden'); 
+    refs.catInfo.classList.remove('is-hidden'); 
     } else {
-        error.style.display = 'block';         
-        breedSelect.style.display = 'none'; 
+        refs.error.style.display = 'block';         
+        refs.breedSelect.style.display = 'none'; 
     }      
 })
  .catch(error => {
-    Notiflix.Notify.failure('Error fetching cat info: ', error);
-    
+    Notiflix.Notify.failure('Error fetching cat info: ', error);    
  })
-.finally(() => {  
-   loader.style.display = 'none';    
+.finally(() => {   
+    refs.loader.style.display = 'none';      
     });
 });
+
+// import { fetchBreeds, fetchCatByBreed } from './cat-api';
+// import Notiflix from 'notiflix';
+
+// const breedSelect = document.getElementById('breed-select');
+// const catInfo = document.querySelector('.cat-info');
+// const loader = document.querySelector('.loader');
+// const error = document.querySelector('.error');
+  
+// fetchBreeds()
+// .then((breeds) => {       
+//     const markup = breeds.map(({id, name}) => {                
+//         return `<option value="${id}">${name}</option>`
+//     }).join("");
+   
+//     breedSelect.insertAdjacentHTML('afterbegin', markup);
+
+//     breedSelect.classList.remove('is-hidden'); 
+
+// })
+// .catch(error => {
+//     Notiflix.Notify.failure('Error fetching cat info: ', error);         
+// })
+// .finally(() => {        
+//     loader.style.display = 'none';  
+// });
+
+// breedSelect.addEventListener('change', event => {
+//     event.preventDefault();
+//     const selectedBreedId = breedSelect.value;  
+//     loader.classList.remove('is-hidden');  
+//     loader.style.display = 'block';  
+//     catInfo.innerHTML = '';
+          
+// fetchCatByBreed(selectedBreedId)
+//  .then((catData) => { 
+//     if (catData.length > 0) {   
+//     const breedInfo = catData[0].breeds[0];  
+//     catInfo.innerHTML = `
+//         <img class= "cat-info-img" src="${catData[0].url}" width="300" alt="${breedInfo.name}">
+//         <div class= "cat">
+//         <h2>${breedInfo.name}</h2>
+//         <p class= "cat-info-text">${breedInfo.description}</p>
+//         <p class= "cat-info-text-bold">Temperament: <span class= "cat-info-text-temp">${breedInfo.temperament}</span></p>
+//         </div>                
+//     `;
+//     catInfo.classList.remove('is-hidden'); 
+//     } else {
+//         error.style.display = 'block';         
+//         breedSelect.style.display = 'none'; 
+//     }      
+// })
+//  .catch(error => {
+//     Notiflix.Notify.failure('Error fetching cat info: ', error);
+    
+//  })
+// .finally(() => {  
+//    loader.style.display = 'none';    
+//     });
+// });
 
